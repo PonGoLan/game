baae505c41451dab2f19cb2775be740211ded462
Aksels Ledins <aksels.ledins@gmail.com>
2018-03-10 15:35:31 +0100

    (game) new struct which handles the score

---
+++ pong/game.go
@@ -0,0 +1,43 @@
+package pong
+
+import (
+	"fmt"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel/text"
+	"golang.org/x/image/font/basicfont"
+)
+
+type Game struct {
+	Score map[int]int
+}
+
+func NewGame() *Game {
+	g := new(Game)
+
+	g.Score = make(map[int]int)
+
+	return g
+}
+
+func (g *Game) AddPoint(playerId int) {
+	g.Score[playerId]++
+}
+
+func (g *Game) DrawScore(win *pixelgl.Window) {
+	basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
+	txt := text.New(pixel.V(770, 940), basicAtlas)
+
+	fmt.Fprintf(txt, "%02d", g.Score[1])
+
+	txt.Draw(win, pixel.IM.Scaled(txt.Orig, 4))
+
+	/// -----------
+
+	txt = text.New(pixel.V(675, 940), basicAtlas)
+
+	fmt.Fprintf(txt, "%02d", g.Score[2])
+
+	txt.Draw(win, pixel.IM.Scaled(txt.Orig, 4))
+}
