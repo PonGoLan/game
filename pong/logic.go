5bd25991dca9e4fc98e5bc300bde9552ab7916ce
Aksels Ledins <aksels.ledins@gmail.com>
2018-03-10 12:43:11 +0100

    (logic) fixed strange behaviour

--- pong/logic.go
+++ pong/logic.go
@@ -1,8 +1,9 @@
 package pong
 
 func BallPlayerCollision(ball *Ball, player *Player) {
-	if ball.PredictedY >= player.Y && ball.PredictedY <= player.Y+player.Size {
-		if ball.PredictedX == player.X {
+	// TODO fix this
+	if ball.PredictedX == player.X || ball.PredictedX == player.X-1 || ball.PredictedX == player.X+1 {
+		if ball.PredictedY >= player.Y && ball.PredictedY <= player.Y+player.Size {
 			ball.VectorX *= -1
 		}
 	}
