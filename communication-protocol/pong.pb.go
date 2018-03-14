d2423fd4b98ca9888a95c97226ed796027fd5506
Aksels Ledins <aksels.ledins@gmail.com>
2018-03-14 13:52:03 +0100

    (protoc) add handshakes + little update

--- communication-protocol/pong.pb.go
+++ communication-protocol/pong.pb.go
@@ -12,6 +12,8 @@ It has these top-level messages:
 	SetPlayerPositionReply
 	GetBallPositionRequest
 	GetBallPositionReply
+	IdentifyPlayerRequest
+	IdentifyPlayerReply
 */
 package pong
 
@@ -36,9 +38,10 @@ var _ = math.Inf
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
 type SetPlayerPositionRequest struct {
-	Id int32 `protobuf:"varint,1,opt,name=Id" json:"Id,omitempty"`
-	X  int32 `protobuf:"varint,2,opt,name=X" json:"X,omitempty"`
-	Y  int32 `protobuf:"varint,3,opt,name=Y" json:"Y,omitempty"`
+	Handshake    string `protobuf:"bytes,1,opt,name=Handshake" json:"Handshake,omitempty"`
+	PlayerNumber int32  `protobuf:"varint,2,opt,name=PlayerNumber" json:"PlayerNumber,omitempty"`
+	X            int32  `protobuf:"varint,3,opt,name=X" json:"X,omitempty"`
+	Y            int32  `protobuf:"varint,4,opt,name=Y" json:"Y,omitempty"`
 }
 
 func (m *SetPlayerPositionRequest) Reset()                    { *m = SetPlayerPositionRequest{} }
@@ -46,9 +49,16 @@ func (m *SetPlayerPositionRequest) String() string            { return proto.Com
 func (*SetPlayerPositionRequest) ProtoMessage()               {}
 func (*SetPlayerPositionRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
 
-func (m *SetPlayerPositionRequest) GetId() int32 {
+func (m *SetPlayerPositionRequest) GetHandshake() string {
 	if m != nil {
-		return m.Id
+		return m.Handshake
+	}
+	return ""
+}
+
+func (m *SetPlayerPositionRequest) GetPlayerNumber() int32 {
+	if m != nil {
+		return m.PlayerNumber
 	}
 	return 0
 }
@@ -68,9 +78,9 @@ func (m *SetPlayerPositionRequest) GetY() int32 {
 }
 
 type SetPlayerPositionReply struct {
-	Id int32 `protobuf:"varint,1,opt,name=Id" json:"Id,omitempty"`
-	X  int32 `protobuf:"varint,2,opt,name=X" json:"X,omitempty"`
-	Y  int32 `protobuf:"varint,3,opt,name=Y" json:"Y,omitempty"`
+	PlayerNumber int32 `protobuf:"varint,1,opt,name=PlayerNumber" json:"PlayerNumber,omitempty"`
+	X            int32 `protobuf:"varint,2,opt,name=X" json:"X,omitempty"`
+	Y            int32 `protobuf:"varint,3,opt,name=Y" json:"Y,omitempty"`
 }
 
 func (m *SetPlayerPositionReply) Reset()                    { *m = SetPlayerPositionReply{} }
@@ -78,9 +88,9 @@ func (m *SetPlayerPositionReply) String() string            { return proto.Compa
 func (*SetPlayerPositionReply) ProtoMessage()               {}
 func (*SetPlayerPositionReply) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
 
-func (m *SetPlayerPositionReply) GetId() int32 {
+func (m *SetPlayerPositionReply) GetPlayerNumber() int32 {
 	if m != nil {
-		return m.Id
+		return m.PlayerNumber
 	}
 	return 0
 }
@@ -100,6 +110,7 @@ func (m *SetPlayerPositionReply) GetY() int32 {
 }
 
 type GetBallPositionRequest struct {
+	Handshake string `protobuf:"bytes,1,opt,name=Handshake" json:"Handshake,omitempty"`
 }
 
 func (m *GetBallPositionRequest) Reset()                    { *m = GetBallPositionRequest{} }
@@ -107,9 +118,16 @@ func (m *GetBallPositionRequest) String() string            { return proto.Compa
 func (*GetBallPositionRequest) ProtoMessage()               {}
 func (*GetBallPositionRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
 
+func (m *GetBallPositionRequest) GetHandshake() string {
+	if m != nil {
+		return m.Handshake
+	}
+	return ""
+}
+
 type GetBallPositionReply struct {
-	X int32 `protobuf:"varint,2,opt,name=X" json:"X,omitempty"`
-	Y int32 `protobuf:"varint,3,opt,name=Y" json:"Y,omitempty"`
+	X int32 `protobuf:"varint,1,opt,name=X" json:"X,omitempty"`
+	Y int32 `protobuf:"varint,2,opt,name=Y" json:"Y,omitempty"`
 }
 
 func (m *GetBallPositionReply) Reset()                    { *m = GetBallPositionReply{} }
@@ -131,11 +149,45 @@ func (m *GetBallPositionReply) GetY() int32 {
 	return 0
 }
 
+type IdentifyPlayerRequest struct {
+}
+
+func (m *IdentifyPlayerRequest) Reset()                    { *m = IdentifyPlayerRequest{} }
+func (m *IdentifyPlayerRequest) String() string            { return proto.CompactTextString(m) }
+func (*IdentifyPlayerRequest) ProtoMessage()               {}
+func (*IdentifyPlayerRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{4} }
+
+type IdentifyPlayerReply struct {
+	PlayerNumber int32  `protobuf:"varint,1,opt,name=PlayerNumber" json:"PlayerNumber,omitempty"`
+	Handshake    string `protobuf:"bytes,2,opt,name=Handshake" json:"Handshake,omitempty"`
+}
+
+func (m *IdentifyPlayerReply) Reset()                    { *m = IdentifyPlayerReply{} }
+func (m *IdentifyPlayerReply) String() string            { return proto.CompactTextString(m) }
+func (*IdentifyPlayerReply) ProtoMessage()               {}
+func (*IdentifyPlayerReply) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{5} }
+
+func (m *IdentifyPlayerReply) GetPlayerNumber() int32 {
+	if m != nil {
+		return m.PlayerNumber
+	}
+	return 0
+}
+
+func (m *IdentifyPlayerReply) GetHandshake() string {
+	if m != nil {
+		return m.Handshake
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*SetPlayerPositionRequest)(nil), "pong.SetPlayerPositionRequest")
 	proto.RegisterType((*SetPlayerPositionReply)(nil), "pong.SetPlayerPositionReply")
 	proto.RegisterType((*GetBallPositionRequest)(nil), "pong.GetBallPositionRequest")
 	proto.RegisterType((*GetBallPositionReply)(nil), "pong.GetBallPositionReply")
+	proto.RegisterType((*IdentifyPlayerRequest)(nil), "pong.IdentifyPlayerRequest")
+	proto.RegisterType((*IdentifyPlayerReply)(nil), "pong.IdentifyPlayerReply")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -151,7 +203,10 @@ const _ = grpc.SupportPackageIsVersion4
 type PongerClient interface {
 	// Sends a player position
 	SetPlayerPosition(ctx context.Context, in *SetPlayerPositionRequest, opts ...grpc.CallOption) (*SetPlayerPositionReply, error)
+	// Gets the ball position
 	GetBallPosition(ctx context.Context, in *GetBallPositionRequest, opts ...grpc.CallOption) (*GetBallPositionReply, error)
+	// Get Player number and the handshake which will be required in the other calls
+	IdentifyPlayer(ctx context.Context, in *IdentifyPlayerRequest, opts ...grpc.CallOption) (*IdentifyPlayerReply, error)
 }
 
 type pongerClient struct {
@@ -180,12 +235,24 @@ func (c *pongerClient) GetBallPosition(ctx context.Context, in *GetBallPositionR
 	return out, nil
 }
 
+func (c *pongerClient) IdentifyPlayer(ctx context.Context, in *IdentifyPlayerRequest, opts ...grpc.CallOption) (*IdentifyPlayerReply, error) {
+	out := new(IdentifyPlayerReply)
+	err := grpc.Invoke(ctx, "/pong.Ponger/IdentifyPlayer", in, out, c.cc, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // Server API for Ponger service
 
 type PongerServer interface {
 	// Sends a player position
 	SetPlayerPosition(context.Context, *SetPlayerPositionRequest) (*SetPlayerPositionReply, error)
+	// Gets the ball position
 	GetBallPosition(context.Context, *GetBallPositionRequest) (*GetBallPositionReply, error)
+	// Get Player number and the handshake which will be required in the other calls
+	IdentifyPlayer(context.Context, *IdentifyPlayerRequest) (*IdentifyPlayerReply, error)
 }
 
 func RegisterPongerServer(s *grpc.Server, srv PongerServer) {
@@ -228,6 +295,24 @@ func _Ponger_GetBallPosition_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Ponger_IdentifyPlayer_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IdentifyPlayerRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PongerServer).IdentifyPlayer(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/pong.Ponger/IdentifyPlayer",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PongerServer).IdentifyPlayer(ctx, req.(*IdentifyPlayerRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Ponger_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "pong.Ponger",
 	HandlerType: (*PongerServer)(nil),
@@ -240,6 +325,10 @@ var _Ponger_serviceDesc = grpc.ServiceDesc{
 			MethodName: "GetBallPosition",
 			Handler:    _Ponger_GetBallPosition_Handler,
 		},
+		{
+			MethodName: "IdentifyPlayer",
+			Handler:    _Ponger_IdentifyPlayer_Handler,
+		},
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "pong.proto",
@@ -248,20 +337,26 @@ var _Ponger_serviceDesc = grpc.ServiceDesc{
 func init() { proto.RegisterFile("pong.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 236 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0xe2, 0x2a, 0xc8, 0xcf, 0x4b,
-	0xd7, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x01, 0xb1, 0x95, 0xdc, 0xb8, 0x24, 0x82, 0x53,
-	0x4b, 0x02, 0x72, 0x12, 0x2b, 0x53, 0x8b, 0x02, 0xf2, 0x8b, 0x33, 0x4b, 0x32, 0xf3, 0xf3, 0x82,
-	0x52, 0x0b, 0x4b, 0x53, 0x8b, 0x4b, 0x84, 0xf8, 0xb8, 0x98, 0x3c, 0x53, 0x24, 0x18, 0x15, 0x18,
-	0x35, 0x58, 0x83, 0x80, 0x2c, 0x21, 0x1e, 0x2e, 0xc6, 0x08, 0x09, 0x26, 0x30, 0x97, 0x31, 0x02,
-	0xc4, 0x8b, 0x94, 0x60, 0x86, 0xf0, 0x22, 0x95, 0x5c, 0xb8, 0xc4, 0xb0, 0x98, 0x53, 0x90, 0x53,
-	0x49, 0x92, 0x29, 0x12, 0x5c, 0x62, 0xee, 0xa9, 0x25, 0x4e, 0x89, 0x39, 0x39, 0x68, 0x6e, 0x51,
-	0x32, 0xe2, 0x12, 0xc1, 0x90, 0x01, 0x99, 0x8e, 0xc7, 0x34, 0xa3, 0x35, 0x8c, 0x5c, 0x6c, 0x01,
-	0x40, 0x4f, 0xa6, 0x16, 0x09, 0x05, 0x73, 0x09, 0x62, 0x38, 0x4f, 0x48, 0x4e, 0x0f, 0x1c, 0x1c,
-	0xb8, 0xfc, 0x2f, 0x25, 0x83, 0x53, 0x1e, 0x68, 0xb3, 0x12, 0x83, 0x90, 0x2f, 0x17, 0x3f, 0x9a,
-	0x9b, 0x84, 0xa0, 0x5a, 0xb0, 0x7b, 0x42, 0x4a, 0x0a, 0x87, 0x2c, 0xd8, 0x38, 0x27, 0x03, 0x2e,
-	0xe9, 0xcc, 0x7c, 0xbd, 0xf4, 0xa2, 0x82, 0x64, 0xbd, 0xd4, 0x8a, 0xc4, 0xdc, 0x82, 0x9c, 0xd4,
-	0x62, 0xbd, 0x8c, 0xd4, 0x9c, 0x9c, 0xfc, 0xf2, 0xfc, 0xa2, 0x9c, 0x14, 0x27, 0x7e, 0x0f, 0x10,
-	0x3b, 0x1c, 0xc4, 0x0e, 0x00, 0x45, 0x60, 0x00, 0x63, 0x12, 0x1b, 0x38, 0x26, 0x8d, 0x01, 0x01,
-	0x00, 0x00, 0xff, 0xff, 0xca, 0xb1, 0x8b, 0x56, 0xd7, 0x01, 0x00, 0x00,
+	// 321 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0x94, 0x52, 0x4d, 0x4f, 0xc2, 0x40,
+	0x10, 0x75, 0x0b, 0x92, 0x30, 0x21, 0x12, 0x57, 0xc5, 0x5a, 0x88, 0x31, 0x7b, 0xf2, 0xd4, 0x18,
+	0x4c, 0xfc, 0x01, 0x5c, 0x44, 0x13, 0x4d, 0x53, 0x0e, 0x94, 0x63, 0xb1, 0x23, 0x34, 0x2e, 0xdd,
+	0xda, 0x2e, 0x91, 0xfe, 0x73, 0x8f, 0x76, 0x5b, 0x88, 0xe9, 0x57, 0xa2, 0xb7, 0xd9, 0x7d, 0x33,
+	0xef, 0xbd, 0x7d, 0xb3, 0x00, 0xa1, 0x08, 0x56, 0x66, 0x18, 0x09, 0x29, 0x68, 0x5b, 0xd5, 0x6c,
+	0x07, 0xfa, 0x0c, 0xa5, 0xc5, 0xdd, 0x04, 0x23, 0x4b, 0xc4, 0xbe, 0xf4, 0x45, 0x60, 0xe3, 0xe7,
+	0x16, 0x63, 0x49, 0x47, 0xd0, 0x9d, 0xba, 0x81, 0x17, 0xaf, 0xdd, 0x0f, 0xd4, 0xc9, 0x0d, 0xb9,
+	0xed, 0xda, 0xbf, 0x17, 0x94, 0x41, 0x2f, 0x1f, 0x7b, 0xdd, 0x6e, 0x96, 0x18, 0xe9, 0x5a, 0xda,
+	0x70, 0x6c, 0x17, 0xee, 0x68, 0x0f, 0x88, 0xa3, 0xb7, 0x32, 0x80, 0x38, 0xea, 0xb4, 0xd0, 0xdb,
+	0xf9, 0x69, 0xc1, 0x1c, 0x18, 0xd4, 0x28, 0x87, 0x3c, 0xa9, 0x30, 0x93, 0x26, 0x66, 0xad, 0xc0,
+	0xdc, 0x3a, 0x30, 0x3f, 0xc0, 0xe0, 0x11, 0xe5, 0xc4, 0xe5, 0xfc, 0x5f, 0x2f, 0x62, 0x63, 0x38,
+	0xaf, 0xcc, 0x29, 0x3f, 0x99, 0x16, 0x29, 0x68, 0x69, 0x07, 0xad, 0x4b, 0xb8, 0x78, 0xf2, 0x30,
+	0x90, 0xfe, 0x7b, 0x92, 0xfb, 0xdb, 0x4b, 0xb1, 0x39, 0x9c, 0x95, 0x81, 0xbf, 0xbe, 0xad, 0xe0,
+	0x52, 0x2b, 0xb9, 0x1c, 0x7f, 0x13, 0xe8, 0x58, 0xe9, 0xea, 0xd2, 0xc6, 0x19, 0x9c, 0x56, 0x22,
+	0xa4, 0xd7, 0x66, 0xb6, 0xe4, 0xa6, 0xad, 0x1a, 0xa3, 0x46, 0x3c, 0xf5, 0xc7, 0x8e, 0xe8, 0x0b,
+	0xf4, 0x4b, 0x29, 0xd0, 0xfd, 0x48, 0x7d, 0xa8, 0x86, 0xd1, 0x80, 0xe6, 0x74, 0xcf, 0x70, 0x52,
+	0xcc, 0x81, 0x0e, 0xf3, 0xfe, 0xda, 0xd8, 0x8c, 0xab, 0x7a, 0x30, 0xe3, 0x9a, 0xdc, 0xc1, 0xd0,
+	0x17, 0xe6, 0x2a, 0x0a, 0xdf, 0x4c, 0xdc, 0xb9, 0x9b, 0x90, 0x63, 0x6c, 0xae, 0x91, 0x73, 0xf1,
+	0x25, 0x22, 0xee, 0x4d, 0xfa, 0x53, 0x55, 0xcf, 0x55, 0x6d, 0xa9, 0x2f, 0x6e, 0x91, 0x65, 0x27,
+	0xfb, 0xeb, 0xf7, 0x3f, 0x01, 0x00, 0x00, 0xff, 0xff, 0x73, 0x94, 0x33, 0x44, 0xf9, 0x02, 0x00,
+	0x00,
 }
