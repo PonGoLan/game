2f8b910024b0bde04238cf677cbe4bcdeee23b9e
Aksels Ledins <aksels.ledins@gmail.com>
2018-03-14 12:38:43 +0100

    (client) v0.1

--- client/main.go
+++ client/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"log"
 	"time"
 
-	"github.com/PonGoLan/pong-go-lan/pong"
+	"golang.org/x/net/context"
+
+	pb "github.com/PonGoLan/game/communication-protocol"
+	pong "github.com/PonGoLan/game/shared"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
 )
 
 func run() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewPongerClient(conn)
+
+	ctx := context.Background()
+	// defer cancel()
+
 	cfg := pixelgl.WindowConfig{
-		Title:  "PonGoLan",
+		Title:  "PonGoLan - Client",
 		Bounds: pixel.R(0, 0, 1024, 768),
 		VSync:  true,
 	}
@@ -53,10 +74,28 @@ func run() {
 
 		select {
 		case <-aTick:
-			pong.BallPlayerCollision(ball, player1)
-			pong.BallPlayerCollision(ball, player2)
-			ball.Move(game)
+			// pong.BallPlayerCollision(ball, player1)
+			// pong.BallPlayerCollision(ball, player2)
+			// ball.Move(game)
 			player1.HandleWindowEvents(win)
+
+			_, err := c.SetPlayerPosition(ctx, &pb.SetPlayerPositionRequest{
+				Id: 1,
+				X:  int32(player1.X),
+				Y:  int32(player1.Y),
+			})
+			if err != nil {
+				log.Printf("could not set pos: %v", err)
+			}
+
+			r, err := c.GetBallPosition(ctx, &pb.GetBallPositionRequest{})
+			if err != nil {
+				log.Printf("could not get ball position: %v", err)
+			}
+			if r != nil {
+				ball.SetPosition(int(r.X), int(r.Y))
+			}
+
 			player2.HandleWindowEvents(win)
 		default:
 		}
