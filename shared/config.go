6eda5f57546740ed2a4fb20eb68930d5b824d3cf
Aksels Ledins <aksels.ledins@gmail.com>
2018-03-15 13:38:20 +0100

    (shared) moved window title inside config

--- shared/config.go
+++ shared/config.go
@@ -1,12 +1,15 @@
 package pong
 
 import (
+	"fmt"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 )
 
 type config struct {
 	Scale        float64
+	WindowTitle  string
 	WindoWConfig pixelgl.WindowConfig
 }
 
@@ -16,8 +19,9 @@ func init() {
 	c = new(config)
 
 	c.Scale = 10
+	c.WindowTitle = "PonGoLan"
 	c.WindoWConfig = pixelgl.WindowConfig{
-		Title:  "PonGoLan - Client",
+		Title:  c.WindowTitle,
 		Bounds: pixel.R(0, 0, 1024, 768),
 		VSync:  true,
 	}
@@ -25,4 +29,8 @@ func init() {
 
 func GetConfig() *config {
 	return c
+}
+
+func (ci *config) GetTitle(frames int) string {
+	return fmt.Sprintf("%s | FPS: %d", ci.WindowTitle, frames)
 }
