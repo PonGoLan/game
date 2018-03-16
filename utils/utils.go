c64527b72d1ffe9723c06bc5132b8e1a5451cc51
Aksels Ledins <aksels.ledins@gmail.com>
2018-03-16 14:24:37 +0100

    (utils) +CreateRandomHandshake

---
+++ utils/utils.go
@@ -0,0 +1,21 @@
+package pongutils
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func CreateRandomHandshake() string {
+	n := 10
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
