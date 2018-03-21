c06beadb961075790fc29ee53862daffbe468773
Aksels Ledins <aksels.ledins@gmail.com>
2018-03-21 11:29:11 +0100

    (server) display : a better ui

--- authoritative-server/display/display.go
+++ authoritative-server/display/display.go
@@ -9,11 +9,13 @@ import (
 	tm "github.com/buger/goterm"
 )
 
+const dashdashdash = "------------------------"
+
 func PrintGeneralInformations(im *instances.InstancesManager) {
 	const format = "%v\t%v\n"
 	w := tabwriter.NewWriter(os.Stdout, 0, 8, 2, ' ', 1)
 	fmt.Fprintf(w, format, "Number of instances", "Number of players")
-	fmt.Fprintf(w, format, "-----------------", "-----------------")
+	fmt.Fprintf(w, format, dashdashdash, dashdashdash)
 	fmt.Fprintf(w, format, im.NumberOfInstances(), 0)
 	w.Flush()
 	fmt.Printf("\n\n")
@@ -31,7 +33,7 @@ func Print() {
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 8, 2, ' ', 1)
 	fmt.Fprintf(w, format, "ID", "Number of players", "Ticks")
-	fmt.Fprintf(w, format, "-----------------", "-----------------", "-----------------")
+	fmt.Fprintf(w, format, dashdashdash, dashdashdash, dashdashdash)
 	for _, instance := range instancesManager.GetInstances() {
 		fmt.Fprintf(w, format, instance.GetRoomName(), instance.GetNumberOfPlayersConnected(), instance.GetTicks())
 	}
